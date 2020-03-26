6d9b908639da2818f24d04383fa3ce4d7626665f
hooney731@gmail.com <hooney731@naver.com>
2020-03-26 10:36:19 +0900

    initial commit

---
+++ ESLint/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'plugin:prettier/recommanded'],
+  rules: {},
+};
